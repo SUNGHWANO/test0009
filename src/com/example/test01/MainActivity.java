be4e042b85acc68ab88115bfde3c618eda3949d9
Administrator <Administrator@MSDN-SPECIAL>
2014-06-29 21:53:19 +0900

    sdf

---
+++ src/com/example/test01/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.test01;
+
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap {
+	
+	
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.init();
+				
+		appView.getSettings().setJavaScriptEnabled(true);
+		finish finish = new finish(this, appView);
+		appView.addJavascriptInterface(finish, "finish");
+		
+		super.loadUrl("file:///android_asset/www/main.html");
+		
+	}
+	
+
+	
+}
+
+
+
---
+++ src/com/example/test01/finish.java
@@ -0,0 +1,46 @@
+package com.example.test01;
+
+import org.apache.cordova.DroidGap;
+import org.apache.cordova.Notification;
+
+import android.support.v4.app.NotificationCompat;
+
+public class finish {
+	private WebView mAppView;
+	private DroidGap mGap;
+	//private MainActivity ma;
+	
+	public finish(DroidGap gap, WebView view){
+		this.mAppView = view;
+		this.mGap = gap;
+	}
+	
+	@android.webkit.JavascriptInterface
+	public void notif() {
+		NotificationManager notimng = (NotificationManager)mGap.getSystemService(mGap.NOTIFICATION_SERVICE);
+    PendingIntent pIntent = PendingIntent.getActivity(mGap, 0, 
+    		new Intent(mGap,MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+    
+   Notification notice = new NotificationCompat.Builder(mGap)
+    .setContentTitle("제목")
+    .setContentText("내용")
+    .setSmallIcon(R.drawable.ic_launcher)
+    .setTicker("안녕")
+    .setAutoCancel(true)
+    .setContentIntent(pIntent)
+    .build();
+    		
+    notimng.notify(0, notice);
+    
+		/*
+  	if ( notimng != null ) {
+			notimng.cancel(0);
+		}
+  	*/
+    
+    Vibrator vibe = (Vibrator)mGap.getSystemService(mGap.VIBRATOR_SERVICE);         
+    vibe.vibrate(1000);
+    
+	}
+
+}
