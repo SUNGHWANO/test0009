897b39fbbf659d245ec0ba76a78787e6997cafd5
bit <bit@bit-Samsung-DeskTop-System>
2014-06-30 10:29:26 +0900

    k

--- www/js/main.js
+++ www/js/main.js
@@ -23,7 +23,7 @@ var db = window.openDatabase("Database", "1.0", "LogDB", 2 * 1024 * 1024);
 
 $(document).ready(function(){
 	startIcon = $('#start').html();
-	
+	showConfirm();
 	loadMainIcon();
 	
 	dragdrop_doing();
